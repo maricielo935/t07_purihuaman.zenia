6d9c826bba1d0300a963f7dd9525f849cba79dd1
maricielo935 <57023493+maricielo935@users.noreply.github.com>
2019-11-24 15:37:19 -0500

    Se agrego el lote.err.bat

---
+++ lote.err.bat
@@ -0,0 +1,24 @@
+python purihuaman/bucles.mientras/bucle.mientras1.py 20 22 20 20
+python purihuaman/bucles.mientras/bucle.mientras2.py 20 20
+python purihuaman/bucles.mientras/bucle.mientras3.py 100 150 560 482
+python purihuaman/bucles.mientras/bucle.mientras4.py 65 37 53 45
+python purihuaman/bucles.mientras/bucle.mientras5.py 70 7
+
+python purihuaman/bucles.para/bucle.para1.py A S F T
+python purihuaman/bucles.para/bucle.para2.py D L P O V X
+python purihuaman/bucles.para/bucle.para3.py Z N I L
+python purihuaman/bucles.para/bucle.para4.py H G C N S W
+python purihuaman/bucles.para/bucle.para5.py U Y E M L V R
+
+python purihuaman/bucles.iteracion/bucle.iteracion1.py 5.8
+python purihuaman/bucles.iteracion/bucle.iteracion2.py 4.7
+python purihuaman/bucles.iteracion/bucle.iteracion3.py 3.1
+python purihuaman/bucles.iteracion/bucle.iteracion4.py 7.0
+python purihuaman/bucles.iteracion/bucle.iteracion5.py 19.4
+
+python purihuaman/bucles.iteracion.en.rango/bucle.iteracion.en.rango1.py 9.0
+python purihuaman/bucles.iteracion.en.rango/bucle.iteracion.en.rango2.py 34.2
+python purihuaman/bucles.iteracion.en.rango/bucle.iteracion.en.rango3.py 6.5
+python purihuaman/bucles.iteracion.en.rango/bucle.iteracion.en.rango4.py 7.6
+python purihuaman/bucles.iteracion.en.rango/bucle.iteracion.en.rango5.py 9.7
+
